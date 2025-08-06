ccf5fce260eea143edb08dc73e3afd713381e5f5
Vishal Kharbanda <vishalkharbanda777@gmail.com>
2025-08-06 22:54:25 +0530

    Bugs fixed : Project Completed

--- Theme-Switcher/src/components/Card.jsx
+++ Theme-Switcher/src/components/Card.jsx
@@ -1,15 +1,15 @@
-import React from 'react';
+import React from 'react'
 
 export default function Card() {
     return (
         <div className="w-full bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
             <a href="/">
-                <img className="p-8 rounded-t-lg" src="https://images.pexels.com/photos/18264716/pexels-photo-18264716/free-photo-of-man-people-laptop-internet.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="product_image1" />
+                <img className="p-8 rounded-t-lg" src="https://images.unsplash.com/photo-1534972195531-d756b9bfa9f2?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTZ8fGNvZGluZ3xlbnwwfHwwfHx8MA%3D%3D" alt="product_image1" />
             </a>
             <div className="px-5 pb-5">
                 <a href="/">
                     <h5 className="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">
-                        Apple Watch Series 7 GPS, Aluminium Case, Starlight Sport
+                       Elegance is when the inside is as beautiful as the past outside
                     </h5>
                 </a>
                 <div className="flex items-center mt-2.5 mb-5">
