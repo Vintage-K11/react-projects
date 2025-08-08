0065ca4a3487a3aaa42bb813fbc5fa8dee176824
Vishal Kharbanda <vishalkharbanda777@gmail.com>
2025-08-08 19:55:38 +0530

    Created Project using Redux

---
+++ ReduxToolkitTodo/src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import AddTodo from './components/AddTodo.jsx'
+import Todos from './components/Todos.jsx'
+
+function App() {
+ 
+
+  return (
+    <>
+      hello
+      <AddTodo />
+      <Todos/>
+    </>
+  )
+}
+
+export default App
