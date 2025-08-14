8807dc6e11d24333d1be83611904e21e8395521c
Vishal Kharbanda <vishalkharbanda777@gmail.com>
2025-08-14 23:28:50 +0530

    initiate logout button component

--- Blog-Appwrite-Project/src/components/Header/LogoutBtn.jsx
+++ Blog-Appwrite-Project/src/components/Header/LogoutBtn.jsx
@@ -1,1 +1,17 @@
 import React from "react";
+import { useDispatch } from "react-redux";
+import authService from "../../appwrite/config";
+import { logout } from "../../store/AuthSlice";
+
+export default function LogoutBtn() {
+    const dispatch = useDispatch();
+    const logoutHandler = () => { 
+        authService.logout().then(() => {
+            dispatch(logout());
+        })
+    }
+
+    return (
+        <div>LogoutBtn</div>
+    )
+}
