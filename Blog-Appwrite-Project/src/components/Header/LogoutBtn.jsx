81abe53f4f500b5db4f49c219046fba13feeef28
Vishal Kharbanda <vishalkharbanda777@gmail.com>
2025-08-15 21:44:41 +0530

    create Header page and LogoutBtn

--- Blog-Appwrite-Project/src/components/Header/LogoutBtn.jsx
+++ Blog-Appwrite-Project/src/components/Header/LogoutBtn.jsx
@@ -12,6 +12,8 @@ export default function LogoutBtn() {
     }
 
     return (
-        <div>LogoutBtn</div>
+        <button className="inline-block px-6 py-2 duration-200 hover:bg-blue-100 rounded-full">
+            Logout
+        </button>
     )
 }
